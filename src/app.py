a49d26a57482713e9cb240ede571fd57420bba54
TheMadMike <m.gibas2407@gmail.com>
2020-07-26 21:15:38 +0200

    Add flask api code

---
+++ src/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask.json import jsonify
+app = Flask(__name__)
+
+from .scraper.scrape import scrape_for_names
+
+@app.route('/<username>', methods=['GET'])
+def get_pinned_repo_names(username):
+    url = f'https://github.com/{username}/'
+    names = scrape_for_names(url)
+    return jsonify({ 'pinned_repos_names' : [name for name in names] } )
